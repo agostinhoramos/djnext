3819f13fd762f0dc42d1369912ee63c7b306e70b
Agostinho Ramos <agostinhopina095@gmail.com>
2023-06-20 20:57:14 +0100

    Setting up backend and frontend source code

---
+++ .build/cmd.sh
@@ -0,0 +1,19 @@
+# Required list
+sudo apt install python3.10-venv
+
+cd src && django-admin startproject server
+cd server && python3 manage.py startapp coreapp
+cd ..
+npx create-next-app@latest client --typescript --eslint # Manual setup required
+cd client && npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+python3 -m venv env
+source env/bin/activate
+
+env/bin/python -m pip install pip pip-tools rav --upgrade
+env/bin/rav run install
+rav run freeze
+
+
+# Get start
+rav run server
---
+++ README.md
@@ -0,0 +1,9 @@
+# Get started with DjNext
+
+# In dev
+- localhost:6080
+- localhost:6443
+
+# In Production
+- https://localhost
+- http://localhost
